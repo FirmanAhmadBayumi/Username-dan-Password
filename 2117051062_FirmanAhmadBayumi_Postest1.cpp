5e67794af7448d99bb61f57fbd515cdfa8691486
FIRMAN AHMAD BAYUMI <92419799+FirmanAhmadBayumi@users.noreply.github.com>
2021-11-23 16:43:58 +0700

    Add files via upload

---
+++ 2117051062_FirmanAhmadBayumi_Postest1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+  string npm, tanggal_lahir, bulan_lahir,tahun_lahir;
+  
+  cout << "NPM \t\t: "; cin >> npm;
+  cout << "Tanggal Lahir: "; cin >> tanggal_lahir;
+  cout << "Bulan Lahir\t: "; cin >> bulan_lahir;
+  cout << "Tahun lahir\t: "; cin >> tahun_lahir;
+  
+  cout << "Username : " << npm << endl;
+  cout << "Password : " << tahun_lahir << bulan_lahir << tanggal_lahir << endl;
+  return 0;
+}
